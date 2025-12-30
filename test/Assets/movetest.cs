28b2bca2c25158a175709854e1bf81ecfb269c22
hanabi2356home <hanabira2356@gmail.com>
2025-12-31 03:06:25 +0900

    이동 연산 변경 및 마우스 포인터 위치에 따른 회전값 변경

--- test/Assets/movetest.cs
+++ test/Assets/movetest.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class movetest : MonoBehaviour
 {
     public float moveSpeed = 2.0f;
+    public float rotSpeed = 5.0f;
+    public Camera mainCamera;
+    public LayerMask groundLayer;
     void Start()
     {
         
@@ -13,11 +16,24 @@ public class movetest : MonoBehaviour
     {
         float xx = Input.GetAxis("Horizontal");
         float zz = Input.GetAxis("Vertical");
-        
-        Vector3 temppos = transform.position;
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        /*Debug.Log(xx);*/
+       /* Vector3 temppos = transform.position;
         temppos.x += xx * moveSpeed * Time.deltaTime;
         temppos.z += zz * moveSpeed * Time.deltaTime;
 
-        transform.position = temppos;
+        transform.position = temppos;*/
+
+        transform.position += new Vector3(xx, 0.0f, zz)* Time.deltaTime* moveSpeed;
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer) )
+        {
+            Debug.Log("rayHit\n");
+            Vector3 lookDir = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            Debug.Log(lookDir);
+            transform.LookAt(lookDir);
+        }
+        
     }
 }

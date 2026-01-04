adeae133b51958915e390d80786ea67e1e68a6ab
hanabi2356home <hanabira2356@gmail.com>
2026-01-04 18:41:49 +0900

    대시 추가(코루틴)

--- test/Assets/Script/movetest.cs
+++ test/Assets/Script/movetest.cs
@@ -1,25 +1,51 @@
 using UnityEngine;
-
+using System.Collections;
+using UnityEditorInternal;
 public class movetest : MonoBehaviour
 {
     public float moveSpeed = 2.0f;
     public float rotSpeed = 5.0f;
     public Camera mainCamera;
     public LayerMask groundLayer;
+    public float dashPower = 10.0f;
+    public float dashTime=2.0f;
+    public float dashCoolDown=5.0f;
+
+    Rigidbody rb;
+    bool isDash;
+    bool canDash;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
         float xx = Input.GetAxis("Horizontal");
         float zz = Input.GetAxis("Vertical");
+        moveUpdate(xx, zz);
+        rotUpdate();
+    }
 
+
+    void Update()
+    {
+               
+        
+    }
+    private void moveUpdate(float x, float z)
+    {
+        transform.position += new Vector3(x, 0.0f, z) * moveSpeed * Time.fixedDeltaTime;
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            StartCoroutine(DashCoroutine());
+        }
+    }
+    private void rotUpdate()
+    {
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        transform.position += new Vector3(xx, 0.0f, zz) * moveSpeed*Time.fixedDeltaTime;
 
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
         {
@@ -27,11 +53,21 @@ public class movetest : MonoBehaviour
             transform.LookAt(lookDir);
         }
     }
-
-
-    void Update()
+    IEnumerator DashCoroutine()
     {
-       
+        isDash = true;
+        canDash = false;
+        rb.useGravity = false;
+
+        Vector3 dashDirection = transform.forward;
+        rb.linearVelocity = dashDirection * dashPower;
+
+        yield return new WaitForSeconds(dashTime);
+
+        rb.useGravity = true;
+        isDash = false;
         
+        yield return new WaitForSeconds(dashCoolDown);
+        canDash = true;
     }
 }
